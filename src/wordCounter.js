eba4d7807b1b41049a4d9fef8d8cf50cbf812bdc
dwafotapa <dominique.wafotapa@gmail.com>
2017-11-25 01:18:46 +0100

    Fix eslint errors

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,8 +1,9 @@
 function wordCounter(text, minLength) {
-    const words = text.split(/[\d\s.,;:!\?\-_=+'"@£$%\^&\*()€#\/]/);
-    const wordCounts = words.reduce((hash, word) => {
+    const words = text.split(/[\d\s.,;:!?\-_=+'"@£$%^&*()€#/]/);
+    const wordCounts = words.reduce((accumulator, currentValue) => {
+        const hash = accumulator;
+        const word = currentValue.toLowerCase();
         if (word && word.length >= minLength) {
-            word = word.toLowerCase();
             hash[word] = (hash[word] || 0) + 1;
         }
         return hash;
--- tests/wordCounter.test.js
+++ tests/wordCounter.test.js
@@ -1,12 +1,11 @@
 const wordCounter = require('../src/wordCounter');
+const fileSamples = require('./_samples');
 
 describe('wordCounter', () => {
     const minLength = 2;
-    
-    it('should return the number of occurrences for each word of 2 letters or more in a given text', () => {
-        const samples = require('./_samples');
 
-        samples.forEach((sample) => {
+    it('should return the number of occurrences for each word of 2 letters or more in a given text', () => {
+        fileSamples.forEach((sample) => {
             const inventory = wordCounter(sample.input, minLength);
             expect(inventory).toEqual(sample.expect);
         });
@@ -21,7 +20,7 @@ describe('wordCounter', () => {
 
     it('should return an empty object if input is numbers', () => {
         const samples = ['1', '1 2', '12', '123', '12 345 6789'];
-        
+
         samples.forEach((sample) => {
             const inventory = wordCounter(sample, minLength);
             expect(inventory).toEqual({});
@@ -29,8 +28,8 @@ describe('wordCounter', () => {
     });
 
     it('should return an empty object if input is special characters', () => {
-        const samples = ['!', '! ?', '!?', '???', '!! ??? :;\'\-'];
-        
+        const samples = ['!', '! ?', '!?', '???', '!! ??? :;\'-'];
+
         samples.forEach((sample) => {
             const inventory = wordCounter(sample, minLength);
             expect(inventory).toEqual({});
@@ -39,7 +38,7 @@ describe('wordCounter', () => {
 
     it('should return an empty object if input is words with a length of 1', () => {
         const samples = ['a', 'a e', 'a e i o u y'];
-        
+
         samples.forEach((sample) => {
             const inventory = wordCounter(sample, minLength);
             expect(inventory).toEqual({});
@@ -55,16 +54,21 @@ describe('wordCounter', () => {
     });
 
     it('should return the right number of occurences if input is same words with different cases', () => {
-        const sample = "This is a case test. THIS IS A CASE TEST!!";
-        
+        const sample = 'This is a case test. THIS IS A CASE TEST!!';
+
         const inventory = wordCounter(sample, minLength);
 
-        expect(inventory).toEqual({ this: 2, is: 2, case: 2, test: 2 });
+        expect(inventory).toEqual({
+            this: 2,
+            is: 2,
+            case: 2,
+            test: 2
+        });
     });
 
     it('should return the right number of occurences if input is words with hyphens', () => {
-        const sample = "A moon-shaped pool";
-        
+        const sample = 'A moon-shaped pool';
+
         const inventory = wordCounter(sample, minLength);
 
         expect(inventory).toEqual({ moon: 1, shaped: 1, pool: 1 });
@@ -72,16 +76,24 @@ describe('wordCounter', () => {
 
     it('should return the right number of occurences if input is words with special letters', () => {
         const sample = "Un café au lait, s'il-vous-plaît!";
-        
+
         const inventory = wordCounter(sample, minLength);
 
-        expect(inventory).toEqual({ un: 1, 'café': 1, au: 1, lait: 1, il: 1, vous: 1, 'plaît': 1 });
+        expect(inventory).toEqual({
+            un: 1,
+            café: 1,
+            au: 1,
+            lait: 1,
+            il: 1,
+            vous: 1,
+            plaît: 1
+        });
     });
 
     it('should return the right number of occurences if input is words with a tab or new line', () => {
         const sample = `    Tab
         New line`;
-        
+
         const inventory = wordCounter(sample, minLength);
 
         expect(inventory).toEqual({ tab: 1, new: 1, line: 1 });
